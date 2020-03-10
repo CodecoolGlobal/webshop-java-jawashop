c5e2a61b3ed4e53ec144606cd99a406b2b8a3fd1
gabona07 <bonagabriella1995@gmail.com>
2020-03-10 11:33:53 +0100

    Give unique id to everything

--- src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoMem.java
+++ src/main/java/com/codecool/shop/dao/implementation/ProductCategoryDaoMem.java
@@ -26,17 +26,16 @@ public class ProductCategoryDaoMem implements ProductCategoryDao {
 
     @Override
     public void add(ProductCategory category) {
-        category.setId(data.size() + 1);
         data.add(category);
     }
 
     @Override
-    public ProductCategory find(int id) {
-        return data.stream().filter(t -> t.getId() == id).findFirst().orElse(null);
+    public ProductCategory find(String id) {
+        return data.stream().filter(t -> t.getId().equals(id)).findFirst().orElse(null);
     }
 
     @Override
-    public void remove(int id) {
+    public void remove(String id) {
         data.remove(find(id));
     }
 
