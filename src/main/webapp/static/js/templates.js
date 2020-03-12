d03d27ffcb0defec6bce517dc7c2f7005f9a648a
gabona07 <bonagabriella1995@gmail.com>
2020-03-12 21:20:50 +0100

    Add card design

--- src/main/webapp/static/js/templates.js
+++ src/main/webapp/static/js/templates.js
@@ -20,7 +20,7 @@ export let template = {
         return `
             <div class="col col-sm-12 col-md-6 col-lg-4">
                 <div class="card card-container">
-                    <img src="/static/img/uploads/${product.id}.jpg" alt="Image of the product">
+                    <img src="/static/img/uploads/${product.id}.jpg" class="product-image" alt="Image of the product">
                     <div class="card-header">
                         <h4 class="card-title">${product.name}</h4>
                         <p class="card-text">${product.description}</p>
