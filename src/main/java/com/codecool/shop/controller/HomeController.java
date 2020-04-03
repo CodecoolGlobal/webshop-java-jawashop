d6119478f7b4ca89d4350a841a5fc7e7d1b8c736
Tom PoLáKoSz <polakosz@freemail.hu>
2020-04-03 10:10:13 +0200

    Add Log4J through SLF4J

--- src/main/java/com/codecool/shop/controller/HomeController.java
+++ src/main/java/com/codecool/shop/controller/HomeController.java
@@ -1,10 +1,11 @@
 package com.codecool.shop.controller;
 
 import com.codecool.shop.config.TemplateEngineUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +14,11 @@ import java.io.IOException;
 @WebServlet(urlPatterns = {"/"})
 public class HomeController extends JsonResponseController {
 
+    private final static Logger logger = LoggerFactory.getLogger(HomeController.class);
+
     @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        logger.info("/ called");
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
         engine.process("product/index.html", context, response.getWriter());
