c4eff68e10668c2ece876ea1c9685dca4b9ca77e
Tom PoLáKoSz <polakosz@freemail.hu>
2020-04-08 22:59:52 +0200

    [WIP] Add to cart disabled when user already logged in

--- src/main/webapp/static/js/index/logic.js
+++ src/main/webapp/static/js/index/logic.js
@@ -1,26 +1,27 @@
 import { dataHandler } from "./data_handler.js";
 import { ui } from "./ui.js";
+import { logic as user } from "../user/logic.js";
 
 export let logic = {
     init: function() {
         ui.init();
 
         dataHandler.getAllProduct(function(response) {
-            ui.renderProductsPage(response);
+            ui.renderProductsPage(response, user.isAuthenticated());
             logic.__addClickEventToAddToCartButtons();
         });
     },
 
     filterByCategory: function(categoryId) {
         dataHandler.getProductsByCategory(categoryId, function(response) {
-            ui.renderProductsPage(response);
+            ui.renderProductsPage(response, user.isAuthenticated());
             logic.__addClickEventToAddToCartButtons();
         });
     },
 
     filterBySupplier: function(supplierId) {
         dataHandler.getProductsBySupplier(supplierId, function(response) {
-            ui.renderProductsPage(response);
+            ui.renderProductsPage(response, user.isAuthenticated());
             logic.__addClickEventToAddToCartButtons();
         });
     },
--- src/main/webapp/static/js/index/templates.js
+++ src/main/webapp/static/js/index/templates.js
@@ -1,5 +1,5 @@
 export let template = {
-    forProduct: function (product) {
+    forEnabledProduct: function (product) {
         return `
             <div class="col col-sm-12 col-md-6 col-lg-4">
                 <div class="card card-container card-vertical">
@@ -20,4 +20,26 @@ export let template = {
             </div>
         `
     },
-}
+
+    forDisabledProduct: function (product) {
+        return `
+            <div class="col col-sm-12 col-md-6 col-lg-4">
+                <div class="card card-container card-vertical">
+                    <img src="/static/img/uploads/${product.id}.jpg" class="product-image" alt="Image of the product">
+                    <div class="card-header">
+                        <h4 class="card-title">${product.name}</h4>
+                        <p class="card-text">${product.description}</p>
+                    </div>
+                    <div class="card-body">
+                        <div class="card-text">
+                            <p class="lead">${product.price.toFixed(1)} ${product.currency.code}</p>
+                            <button type="button" class="btn btn-secondary disabled" data-toggle="tooltip" data-placement="top" title="Login required">
+                                Add to cart
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `
+    },
+};
