18aad34c4218978903befc0f90d85d52587f700a
Tom PoLáKoSz <polakosz@freemail.hu>
2020-04-09 08:18:02 +0200

    Successful logout redirect to login page

--- src/main/webapp/static/js/logout/logic.js
+++ src/main/webapp/static/js/logout/logic.js
@@ -1,5 +1,6 @@
 import { dataHandler } from "./data_handler.js";
 import { navbar } from "../navbar/logic.js";
+import { logic as login } from "../login/logic.js";
 import { logic as user } from "../user/logic.js";
 
 export let logic = {
@@ -8,6 +9,7 @@ export let logic = {
             if (response === "Successful logout") {
                 navbar.loggedOut();
                 user.setLoggedOut();
+                login.navigate();
             }
         });
     },
