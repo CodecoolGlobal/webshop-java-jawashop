73f4b05bb2f6f4162e724cbce1e330d1a5c64de1
Tom PoLáKoSz <polakosz@freemail.hu>
2020-04-23 13:04:54 +0200

    Remove prefilled form data

--- src/main/webapp/static/js/payment/templates.js
+++ src/main/webapp/static/js/payment/templates.js
@@ -26,7 +26,7 @@ export let template = {
                                 <div class="form-group col">
                                     <label for="inputCardNumber">Card Number</label>
                                     <i class="fas fa-question-circle d-sm-inline d-none" data-toggle="tooltip" data-placement="top" title="e.g. 1234 1234 1234 1234"></i>
-                                    <input type="text" name="card_number" class="form-control" id="inputCardNumber" placeholder="e.g. 1234 1234 1234 1234" minlength="12" maxlength="19" required value="1234 1234 1234 1234">
+                                    <input type="text" name="card_number" class="form-control" id="inputCardNumber" placeholder="e.g. 1234 1234 1234 1234" minlength="12" maxlength="19" required>
                                     <small class="form-text text-muted d-inline d-sm-none">
                                         e.g. 1234 1234 1234 1234
                                     </small>
@@ -36,7 +36,7 @@ export let template = {
                                 <div class="form-group col">
                                     <label for="inputCardOwner">Card Owner Name</label>
                                     <i class="fas fa-question-circle d-sm-inline d-none" data-toggle="tooltip" data-placement="top" title="e.g. John Doe"></i>
-                                    <input type="text" name="card_owner" class="form-control" id="inputCardOwner" placeholder="e.g. John Doe" required value="John Doe">
+                                    <input type="text" name="card_owner" class="form-control" id="inputCardOwner" placeholder="e.g. John Doe" required>
                                     <small class="form-text text-muted d-inline d-sm-none">
                                         e.g. John Doe
                                     </small>
@@ -46,7 +46,7 @@ export let template = {
                                 <div class="form-group col-md-6">
                                     <label for="inputCardExpiringYear">Card Expiring Date</label>
                                     <i class="fas fa-question-circle d-sm-inline d-none" data-toggle="tooltip" data-placement="top" title="e.g. 01/20 for 2020 Januar"></i>
-                                    <input type="text" name="card_expire" class="form-control" id="inputCardExpiringYear" placeholder="MM/YY" minlength="5" maxlength="5" required value="01/20">
+                                    <input type="text" name="card_expire" class="form-control" id="inputCardExpiringYear" placeholder="MM/YY" minlength="5" maxlength="5" required>
                                     <small class="form-text text-muted d-inline d-sm-none">
                                         e.g. 01/20 for 2020 Januar
                                     </small>
@@ -54,7 +54,7 @@ export let template = {
                                 <div class="form-group col-md-6">
                                     <label for="inputCardCode">Card Code</label>
                                     <i class="fas fa-question-circle d-sm-inline d-none" data-toggle="tooltip" data-placement="top" title="e.g. 123"></i>
-                                    <input type="number" name="card_code" class="form-control" id="inputCardCode" placeholder="CVC/CVV Code" min="100" max="999" required value="722">
+                                    <input type="number" name="card_code" class="form-control" id="inputCardCode" placeholder="CVC/CVV Code" min="100" max="999" required>
                                     <small class="form-text text-muted d-inline d-sm-none">
                                         e.g. 123
                                     </small>
